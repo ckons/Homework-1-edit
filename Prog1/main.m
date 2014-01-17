52ae351c7c4bb5dee0355c3fcb0b14b8ae9e7856
Dino <dinokonstantakis@dinos-macbook-pro.local>
2014-01-17 12:31:47 -0800

    Initial Commit

---
+++ Prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Prog1
+//
+//  Created by Dino Konstantakis on 1/17/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
